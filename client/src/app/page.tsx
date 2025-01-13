21840e3bbe8e8c0cd14697677fcbf90d54d05783
sahilchauhan0603 <sahilchauhan0603@gmail.com>
2025-01-14 02:29:30 +0530

    backend removed and login signup

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,7 +1,18 @@
+"use client"
 import HomePage from "../components/homepage";
+import { useEffect } from "react";
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs";
 
 export default function Page() {
+
+  const { user } = useKindeBrowserClient();
+  useEffect(() => {
+    console.log(user);
+  }, [user]);
+
   return (
-    <HomePage/>
+    <div>
+      <HomePage/>
+    </div>
   );
 }
